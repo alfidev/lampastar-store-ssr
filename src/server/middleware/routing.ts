2fa45f5d9f0823b724c4e2ce28575cb42e6c7279
AlexeyFirsaev <aleksfordev@gmail.com>
2022-07-03 19:10:52 +0300

    feat: [LS-6] CI/CD

--- src/server/middleware/routing.ts
+++ src/server/middleware/routing.ts
@@ -4,32 +4,29 @@ import { renderReactAsync } from "server/ssr/renderReactAsync";
 
 /** Defines the server routings. */
 export function useRouting(app: Express) {
+  app.get("/about", async (req, res) => {
+    const model: ExampleModel = {
+      id: 123,
+      message: "This data came from the server",
+    };
 
-    app.get("/sample-page-1", async (req, res) => {
+    try {
+      const html = await renderReactAsync(req.url);
+      return res.status(200).contentType("text/html").send(html);
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send("Internal server error");
+    }
+  });
 
-        const model: ExampleModel = {
-            id: 123,
-            message: "This data came from the server"
-        };
-
-        try {
-            const html = await renderReactAsync(req.url, model);
-            return res.status(200).contentType("text/html").send(html);
-        }
-        catch {
-            return res.status(500).send("Internal server error");
-        }
-    });
-
-    /**
-     * put other routes here like:
-     * 
-     * app.post("/foobar", (req, res) => {
-     *      
-     *     ...stuff
-     * 
-     * })
-     * 
-     */
-
+  /**
+   * put other routes here like:
+   *
+   * app.post("/foobar", (req, res) => {
+   *
+   *     ...stuff
+   *
+   * })
+   *
+   */
 }
