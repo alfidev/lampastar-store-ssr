32c3bda10ddfe2630864990aec7b88aa94d89d45
AlexeyFirsaev <aleksfordev@gmail.com>
2022-08-28 19:06:14 +0300

    feat: [LS-27] Каталог

--- src/client/Index.tsx
+++ src/client/Index.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import {hydrate} from "react-dom";
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 function WrappedApp(): JSX.Element {
-    return (
-        <BrowserRouter >
-            <App />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
 
-hydrate(<WrappedApp/>, document.getElementById("root"));
+if (process.env.NODE_ENV === 'development') {
+  render(<WrappedApp />, document.getElementById('root'));
+} else {
+  hydrate(<WrappedApp />, document.getElementById('root'));
+}
--- src/client/modules/Catalog/containers/CatalogCategories.tsx
+++ src/client/modules/Catalog/containers/CatalogCategories.tsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import { useCatalog } from '../hooks/useCatalog'
-import { PageTitle } from '@layouts/Lampastar'
+import React from 'react';
+import { PageTitle } from '@layouts/Lampastar';
 
 export const CatalogCategories = () => {
-  const { isLoading, list } = useCatalog()
-
-  if (isLoading) return null
-
   return (
     <>
       <PageTitle>Каталог</PageTitle>
-      {list.map(({ name }) => name)}
     </>
-  )
-}
+  );
+};
