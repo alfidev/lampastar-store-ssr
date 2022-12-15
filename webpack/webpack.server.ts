a69dbfded47593898a3bde846d9793f5130eb597
AlexeyFirsaev <aleksfordev@gmail.com>
2022-12-16 00:39:12 +0300

    feat: [LS-48] Доработка каруселей и слайдеров

--- webpack/webpack.server.ts
+++ webpack/webpack.server.ts
@@ -1,67 +1,63 @@
-import { DefinePlugin, Configuration } from "webpack";
-import "webpack-dev-server";
-import path from "path";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import { merge } from "webpack-merge";
-import createBaseConfig from "./webpack.base";
-import { Env } from "shared/envType";
-import { default as clientConfig } from "./webpack.client";
+import { DefinePlugin, Configuration } from 'webpack';
+import 'webpack-dev-server';
+import path from 'path';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { merge } from 'webpack-merge';
+import createBaseConfig from './webpack.base';
+import { Env } from 'shared/envType';
+import { default as clientConfig } from './webpack.client';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 
 function createServerConfig(): Configuration {
   return {
-    name: "server",
-    target: "node",
-    context: path.resolve(__dirname, "../src/server"),
+    name: 'server',
+    target: 'node',
+    context: path.resolve(__dirname, '../src/server'),
     externalsPresets: {
       node: true,
     },
     ignoreWarnings: [
       {
         module: /express/,
-        message:
-          /Critical\sdependency:\sthe\srequest\sof\sa\sdependency\sis\san\sexpression/,
+        message: /Critical\sdependency:\sthe\srequest\sof\sa\sdependency\sis\san\sexpression/,
       },
     ],
-    entry: "./app.ts",
+    entry: './app.ts',
     output: {
-      path: path.resolve(__dirname, "../dist"),
-      filename: "app.js",
-      publicPath: "./",
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'app.js',
+      publicPath: './',
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: "ts-loader",
-          exclude: [
-            path.resolve(__dirname, "../node_modules"),
-            path.resolve(__dirname, "../src/client/resources"),
-          ],
+          loader: 'ts-loader',
+          exclude: [path.resolve(__dirname, '../node_modules'), path.resolve(__dirname, '../src/client/resources')],
         },
         {
           test: /\.(jpg|png|gif)$/,
-          include: [path.resolve(__dirname, "../src/client/resources/images")],
+          include: [path.resolve(__dirname, '../src/client/resources/images')],
           use: {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "images",
-              name: "[name].[contenthash].[ext]",
+              outputPath: 'images',
+              name: '[name].[contenthash].[ext]',
               emitFile: false,
             },
           },
         },
         {
           test: /\.svg$/,
-          include: [path.resolve(__dirname, "../src/client/resources/images")],
+          include: [path.resolve(__dirname, '../src/client/resources/images')],
           use: {
-            loader: "@svgr/webpack",
+            loader: '@svgr/webpack',
             options: {
               svgoConfig: {
                 plugins: [
                   {
-                    name: "removeViewBox",
+                    name: 'removeViewBox',
                     active: false,
                   },
                 ],
@@ -71,15 +67,19 @@ function createServerConfig(): Configuration {
         },
         {
           test: /\.(woff2|woff|eot|ttf|otf|svg)$/,
-          include: [path.resolve(__dirname, "../src/client/resources/fonts")],
-          use: ["file-loader"],
+          include: [path.resolve(__dirname, '../src/client/resources/fonts')],
+          use: ['file-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
       ],
     },
 
     plugins: [
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ["!public/**"],
+        cleanOnceBeforeBuildPatterns: ['!public/**'],
       }),
 
       new DefinePlugin({
@@ -90,12 +90,12 @@ function createServerConfig(): Configuration {
 }
 
 export default function (e: any) {
-  const MEASURE = !!e["MEASURE"];
+  const MEASURE = !!e['MEASURE'];
 
   const env: Env = {
-    production: !!e["PRODUCTION"],
-    host: process.env.HOST || "0.0.0.0",
-    https: process.env.HTTPS === "true",
+    production: !!e['PRODUCTION'],
+    host: process.env.HOST || '0.0.0.0',
+    https: process.env.HTTPS === 'true',
   };
 
   const baseConfig = createBaseConfig(env);
